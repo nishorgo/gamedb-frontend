e55a91aac64e2401ff8cb111b0222e5941f0d7d0
Abid Hasan <abidhasan.bh@outlook.com>
2024-04-30 10:42:13 +0600

    Built the color mode switch to toggle dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -6,15 +7,15 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`
     }}>
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside" >
             Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main" >
         Main
       </GridItem>
     </Grid>
